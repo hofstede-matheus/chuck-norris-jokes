9f85c706b25b10a3995b913a7786ee005395e115
hofstede-matheus <hofstede.matheus@gmail.com>
2020-12-20 18:41:09 -0300

    chore: configure linter

--- app/src/main/java/com/hofstedematheus/chucknorrisjokes/ui/MainActivity.kt
+++ app/src/main/java/com/hofstedematheus/chucknorrisjokes/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.hofstedematheus.chucknorrisjokes.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hofstedematheus.chucknorrisjokes.R
 
 class MainActivity : AppCompatActivity() {
